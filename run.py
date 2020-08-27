56633f6b5a333f9527826282b7aeebdd3128e0b5
chezapeake <56056591+chezapeake@users.noreply.github.com>
2020-08-27 23:28:03 +1200

    Update
    Added some more, realised that I can't get user input working so changed the design a bit.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 import sqlite3
 
 app = Flask(__name__)
@@ -11,36 +11,105 @@ app = Flask(__name__)
 
 LISTDB = 'list.db'
 
+def fetchMenu(con):
+    films = []
+    cur = con.execute('SELECT film FROM films')
+    for row in cur:
+        films.append(list(row))
+
+    games = []
+    cur = con.execute('SELECT game FROM games')
+    for row in cur:
+        games.append(list(row))
+
+    textbles = []
+    cur = con.execute('SELECT textble FROM textbles')
+    for row in cur:
+        textbles.append(list(row))
+
+    miscs = []
+    cur = con.execute('SELECT misc FROM miscs')
+    for row in cur:
+        miscs.append(list(row))
+
+    return {'films':films, 'games':games, 'textbles':textbles, 'miscs':miscs}
+
 @app.route('/')
 def index():
 
     return render_template('index.html')
 
-@app.route('/output')
-def output():
-    db = sqlite3.connect(LISTDB)
-    print(db)
+@app.route('/addlist')
+def addlist():
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
 
-    films = []
-    cur = db.execute('SELECT film FROM films')
-    for row in cur:
-        films.append(list(row))
+    #for input in request.form:
+    #    if input == 'film':
+    #        details[input] = request.form[input]
+            #Me trying to have user input add into the database
+            #conn = sqlite3 . connect ('list')
+            #cursor = conn.cursor()
+            #s_film = input('film:')
+            #cursor.execute("""INSERT INTO films (film)) VALUES (?)""", (s_film))
+            #conn.commit()
+            #print('Data entered successfully.')
+            #conn . close()
+            #if (conn):
+            #    conn.close()
+            #    print("\nThe SQLite connection is closed.")
 
-    games = []
-    cur = db.execute('SELECT game FROM games')
-    for row in cur:
-        games.append(list(row))
+    #    elif request.form[input] and request.form[input] != '0':
+    #        items[input] = request.form[input]
 
-    textbles = []
-    cur = db.execute('SELECT textble FROM textbles')
-    for row in cur:
-        texts.append(list(row))
 
-    miscs = []
-    cur = db.execute('SELECT misc FROM miscs')
-    for row in cur:
-        miscs.append(list(row)) 
 
-    db.close()
+    return render_template('addlist.html',
+    films=menu['films'],
+    games=menu['games'],
+    textbles=menu['textbles'],
+    miscs=menu['miscs'])
 
-    return render_template('output.html', films=film, games=game, textbles=textble, miscs=misc)
+@app.route('/filmlink')
+def filmlink():
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
+    return render_template('film.html', films=menu['films'])
+
+@app.route('/gamelink')
+def gamelink():
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
+    return render_template('game.html', games=menu['games'])
+
+@app.route('/textlink')
+def textlink():
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
+    return render_template('text.html', textbles=menu['textbles'])
+
+@app.route('/misclink')
+def misclink():
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
+    return render_template('misc.html', miscs=menu['miscs'])
+
+@app.route('/listed')
+def listed():
+
+    print(request.form)
+    con = sqlite3.connect(LISTDB)
+    menu = fetchMenu(con)
+    con.close()
+
+    return render_template(
+    'listed.html',
+    films=menu['films'],
+    games=menu['games'],
+    textbles=menu['textbles'],
+    miscs=menu['miscs'], items='items')
